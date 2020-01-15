c48a0648926ec0aca1830179ef125ed04295c880
Nathaniel Perrin <ngperrin@protonmail.com>
2020-01-15 14:44:29 -0600

    Functioning but needs styling

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,41 @@
+require 'flickraw'
+
+class StaticPagesController < ApplicationController
+	include StaticPagesHelper
+
+		FlickRaw.api_key = ENV['FLICKRAW_API_KEY']
+		FlickRaw.shared_secret = ENV['FLICKRAW_SHARED_SECRET']
+
+
+
+  def home
+  	if !params[:user_id].blank?
+
+  		@user_id = flickr.people.getInfo(:user_id => params[:user_id])
+
+  	     	get_pics_by_id
+
+  	end
+  end
+
+  def groups
+  end
+
+  def search
+  	if !params[:tag].blank?
+  		get_pics_by_tag
+  	end
+  end
+
+  def todayspics
+    get_todays_pics
+  end
+
+  def datesearch
+    if params[:datesearch]
+      search_by_date
+    end
+  end
+
+
+end
---
+++ app/helpers/static_pages_helper.rb
@@ -0,0 +1,7 @@
+module StaticPagesHelper
+
+  def get_pics_by_id
+		@photos = flickr.photos.search(user_id: params[:user_id], per_page: 50)
+	end
+
+end
